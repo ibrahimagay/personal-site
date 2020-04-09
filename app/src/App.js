fc9f9a8f1d0a381234ea2231a664c0abf0f091b1
ibrahimagay <ibrahim.agay@DNB.LOCAL>
2020-04-09 14:31:27 +0300

    sayfalar oluşturuldu

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <Home />
     </div>
   );
 }
---
+++ app/src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './home.css';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+           <h1>Hello</h1>
+    );
+  }
+}
