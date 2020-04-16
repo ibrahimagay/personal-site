9e418e9fa1f30e41dc2dc496147ac2861c5fbc08
ibrahimagay <ibrahim.agay@DNB.LOCAL>
2020-04-17 00:01:55 +0300

    Blog Posts

--- app/src/pages/home/home.js
+++ app/src/pages/home/home.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import './_home.scss';
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: '' };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
+import BlogPosts from '../../components/BlogPosts/blogPosts';
+export default class Home extends React.Component { 
 
   render() {
     return (
       <section className="contentBox">
-        <div className="aboutBox">
+        {/* <div className="aboutBox">
           <h1>- HOME -</h1>
-        </div>
+        </div> */}
+        <BlogPosts/> 
       </section>
     );
   }
